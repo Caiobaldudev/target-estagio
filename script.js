4296682dd3070bc90667d9cfea581462d0db0be7
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-23 15:42:58 -0300

    feat: result 1

---
+++ script.js
@@ -0,0 +1,15 @@
+// 1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+// Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+// Imprimir(SOMA);
+// Ao final do processamento, qual será o valor da variável SOMA?
+
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+  K = K + 1;
+  SOMA = SOMA + K;
+}
+
+console.log(`Resultado 1: ${SOMA}`);
