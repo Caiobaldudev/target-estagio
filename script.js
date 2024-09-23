02dab8482edb2defdc779b6ac27087225439b347
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-23 16:00:20 -0300

    feat: result 4

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ while (K < INDICE) {
 }
 
 console.log(`Resultado 1: ${SOMA}`);
-
+console.log('---------------------------------------');
 // 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
 
 // IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
@@ -39,11 +39,11 @@ function calcFibonacci(num) {
 const numero = 34; // Colocar aqui o número
 
 if (calcFibonacci(numero)) {
-  console.log(`${numero} pertence à sequencia Fibonacci.`);
+  console.log(`Resultado 2: ${numero} pertence à sequencia Fibonacci.`);
 } else {
-  console.log(`${numero} não percente à sequência de Fibonacci.`);
+  console.log(`Resultado 2: ${numero} não percente à sequência de Fibonacci.`);
 }
-
+console.log('---------------------------------------');
 // 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
 // • O menor valor de faturamento ocorrido em um dia do mês;
 // • O maior valor de faturamento ocorrido em um dia do mês;
@@ -100,8 +100,38 @@ const diasAcimaDaMedia = diasComFaturamento.filter(
   (dia) => dia.faturamento > mediaMensal
 ).length;
 
-console.log(`Menor valor de faturamento: ${menorFaturamento}`);
-console.log(`Maior valor de faturamento: ${maiorFaturamento}`);
+console.log(`Resultado 3: Menor valor de faturamento: ${menorFaturamento}`);
+console.log(`Resultado 3: Maior valor de faturamento: ${maiorFaturamento}`);
 console.log(
-  `Número de dias com faturamento acima da média: ${diasAcimaDaMedia}`
+  `Resultado 3: Número de dias com faturamento acima da média: ${diasAcimaDaMedia}`
 );
+
+console.log('---------------------------------------');
+
+
+// 4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+// • SP – R$67.836,43
+// • RJ – R$36.678,66
+// • MG – R$29.229,88
+// • ES – R$27.165,48
+// • Outros – R$19.849,53
+
+// Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.
+
+const faturamento = {
+  SP: 67836.43,
+  RJ: 36678.66,
+  MG: 29229.88,
+  ES: 27165.48,
+  Outros: 19849.53,
+};
+
+const faturamentoTotal = Object.values(faturamento).reduce(
+  (total, valor) => total + valor,
+  0
+);
+
+for (let estado in faturamento) {
+  const percentual = (faturamento[estado] / faturamentoTotal) * 100;
+  console.log(`Resultado 4: ${estado}: ${percentual.toFixed(2)}%`);
+}
