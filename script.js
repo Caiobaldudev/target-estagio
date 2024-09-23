b6ce85d96c1f08fa12eca97d54a06cf21c4a517b
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-23 16:03:42 -0300

    feat: result 5

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ while (K < INDICE) {
 }
 
 console.log(`Resultado 1: ${SOMA}`);
-console.log('---------------------------------------');
+console.log("---------------------------------------");
 // 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
 
 // IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
@@ -43,7 +43,7 @@ if (calcFibonacci(numero)) {
 } else {
   console.log(`Resultado 2: ${numero} não percente à sequência de Fibonacci.`);
 }
-console.log('---------------------------------------');
+console.log("---------------------------------------");
 // 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
 // • O menor valor de faturamento ocorrido em um dia do mês;
 // • O maior valor de faturamento ocorrido em um dia do mês;
@@ -106,8 +106,7 @@ console.log(
   `Resultado 3: Número de dias com faturamento acima da média: ${diasAcimaDaMedia}`
 );
 
-console.log('---------------------------------------');
-
+console.log("---------------------------------------");
 
 // 4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
 // • SP – R$67.836,43
@@ -135,3 +134,24 @@ for (let estado in faturamento) {
   const percentual = (faturamento[estado] / faturamentoTotal) * 100;
   console.log(`Resultado 4: ${estado}: ${percentual.toFixed(2)}%`);
 }
+
+console.log("---------------------------------------");
+
+// 5) Escreva um programa que inverta os caracteres de um string.
+
+// IMPORTANTE:
+// a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+// b) Evite usar funções prontas, como, por exemplo, reverse;
+
+function invertString(str) {
+  let stringInvert = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    stringInvert += str[i];
+  }
+  return stringInvert;
+}
+
+const myStr = "Cacau";
+
+console.log(`Resultado 5: ${invertString(myStr)}`);
